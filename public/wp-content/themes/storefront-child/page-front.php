0388b5268fad3ba4eadbbd23616e9f157978a3a5
Stig <sc@konstellation.dk>
2016-02-01 08:29:54 +0100

    Updated static front

--- public/wp-content/themes/storefront-child/page-front.php
+++ public/wp-content/themes/storefront-child/page-front.php
@@ -2,22 +2,20 @@
 /**
  * Template Name: Front Page
  */
-
 get_header(); ?>
 
-	<div id="primary" class="content-area">
 	<div class="bg"></div>
-		<main id="main" class="site-main" role="main">
-		<div class="logo">
-		<a href="<?php echo site_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri() . '/img/sinus_logo2.png'; ?>" alt="Sinus Headphones company logo" /></a>
-		<h4>sinus-store.dk // headphones & audio</h4>
-		</div>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<div class="page-content">
-					<?php the_content(); ?>
-				</div>
-			<?php endwhile; ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="logo">
+				<img src="<?php echo get_stylesheet_directory_uri() . '/img/sinuslogo_large.png'; ?>" alt="Sinus Headphones company logo" />
+				<h4>HEADPHONES & AUDIO</h4>
+			</div>
+
+			<div class="comingsoon">
+				<h4>Åbner snart... </h4>
+			</div>
 		</main>
 	</div>
 
