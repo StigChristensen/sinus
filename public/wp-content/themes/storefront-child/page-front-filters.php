102df2bb1f562dc85f7467a1f442a6d516cb709c
Stig <sc@konstellation.dk>
2016-03-22 11:36:13 +0100

    wip

--- public/wp-content/themes/storefront-child/page-front-filters.php
+++ public/wp-content/themes/storefront-child/page-front-filters.php
@@ -65,13 +65,19 @@ get_header(); ?>
 
           <?php $args = array( 'post_type' => 'banner', 'posts_per_page' => 1 );
                 $loop = new WP_Query( $args );
-                while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                while ( $loop->have_posts() ) : $loop->the_post();
+
+                $should_show = get_field('showing');
+
+                if ($should_show) { ?>
 
                 <div class="banner">
                   <a href="<?php the_field('banner_link'); ?>" target="_parent">
                     <img src="<?php the_field('banner_image'); ?>" alt="Sinus Store Headphones and Audio Front Page Banner"/>
                   </a>
                 </div>
+
+          <?php } ?>
 
           <?php endwhile; ?>
           </div>
