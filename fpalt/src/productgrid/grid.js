7913d4398e5cb4e3da2a12f4650b20caf7d00bfc
Stig <sc@konstellation.dk>
2016-07-01 12:13:17 +0200

    WIP

--- fpalt/src/productgrid/grid.js
+++ fpalt/src/productgrid/grid.js
@@ -1,3 +1,16 @@
-const $ = jQuery;
+const $ = jQuery || window.jQuery;
 
-
+$(document).on('ready', function() {
+  $.ajax
+  ({
+    type: "GET",
+    url: "https://www.sinus-store.dk/wp-json/posts?type=product&filter[product_cat]=inear&filter[product_cat]=studie&page=2",
+    dataType: 'json',
+    success: function(response){
+      console.log('Response: - ' + JSON.stringify(response));
+    },
+    error: function(response) {
+      console.log('Error: - ' + JSON.stringify(response));
+    }
+  });
+});
